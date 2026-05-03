f80df2d7cb579ea564ce7e5033de83ba452604ba
atho-gitrepo <aungthuheinoo1992@gmail.com>
2026-05-03 12:26:01 +0630

    Update config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+FIREBASE_CRED = os.getenv("FIREBASE_CREDENTIALS")
