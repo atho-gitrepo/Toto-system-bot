8209dcaa2dd52e51e65633c06c6173ce5cae2c50
atho-gitrepo <aungthuheinoo1992@gmail.com>
2026-05-03 12:30:57 +0000

    remove all files

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn app.webhook:app
--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-
-FIREBASE_CRED = os.getenv("FIREBASE_CREDENTIALS")
